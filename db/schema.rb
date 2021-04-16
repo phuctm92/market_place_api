6ca1562c5493042ea3e2bb2c3da8d18513225bb0
Phuc Thai <phuc.tm4192@gmail.com>
2021-04-16 14:14:41 +0700

    Generate product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_082438) do
+ActiveRecord::Schema.define(version: 2021_04_16_061956) do
+
+  create_table "products", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price", precision: 10
+    t.boolean "published"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", null: false
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_04_14_082438) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
