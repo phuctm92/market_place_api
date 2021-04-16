51986776574ebe3a71ad84318584ce6be48e4333
Phuc Thai <phuc.tm4192@gmail.com>
2021-04-16 15:03:20 +0700

    Finishes modeling the product model along with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :create, :update, :destroy]
+      resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
+      resources :products, only: %i[show index]
     end
   end
 end
