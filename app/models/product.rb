ba34371140f76deb13c1f7d0533e10948e4e45a2
Phuc Thai <phuc.tm4192@gmail.com>
2021-04-21 22:10:53 +0700

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
 
   belongs_to :user
+
+  scope :filter_by_title, lambda { |keyword| where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
+  scope :greater_or_equal_to_price, lambda { |price| where('price >= ?', price) }
+  scope :less_or_equal_to_price, lambda { |price| where('price <= ?', price) }
+  scope :recent, lambda { order(:updated_at) }
 end
